7119358a962c8e38cec7efbf61aa42cd94e809e3
NicoBarragan <nbarragan01@gmail.com>
2022-09-20 13:20:52 -0300

    feat(switch.go): created file for 2 dif switch flows

---
+++ src/switch.go
@@ -0,0 +1,27 @@
+package main
+
+import "fmt"
+
+func main() {
+	// For a var without too many conditions this is the best option
+	switch module := 5 % 2; module {
+	case 0:
+		fmt.Println("Is odd")
+	default:
+		fmt.Println("Is Even")
+	}
+
+	// For multiple conds on a var this is better:
+	value := 200
+
+	switch {
+	case value > 100:
+		fmt.Println("Greater than 100")
+	case value < 0:
+		fmt.Println("Less than zero")
+
+	default:
+		fmt.Println("No condition")
+
+	}
+}
