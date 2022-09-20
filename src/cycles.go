5a5aa5e357299045523a81a0c4587be90d8e4d28
NicoBarragan <nbarragan01@gmail.com>
2022-09-20 12:32:07 -0300

    feat(cycles.go): added for dif loops

---
+++ src/cycles.go
@@ -0,0 +1,33 @@
+package main
+
+import "fmt"
+
+func main() {
+
+	// For conditional
+	for i := 0; i <= 10; i++ {
+		fmt.Println(i)
+	}
+
+	fmt.Println("")
+
+	// For while (python way)
+	counter := 0
+	for counter < 10 {
+		fmt.Println(counter)
+		counter++
+	}
+
+	// Task: do an decremental loop
+	for i := 10; i == 0; i-- {
+		fmt.Println(i)
+	}
+
+	// For forever (job) it won't end until Ctrl+c
+	counterForever := 0
+	for {
+		fmt.Println(counterForever)
+		counterForever++
+	}
+
+}
