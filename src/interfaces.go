39f5e733791ba887b3099188dc82ef75b9073d4d
NicoBarragan <nbarragan01@gmail.com>
2022-09-21 18:30:22 -0300

    feat(interfaces.go): created file that interacts w interfaces and implements them

---
+++ src/interfaces.go
@@ -0,0 +1,41 @@
+package main
+
+import "fmt"
+
+/* interfaces are very useful when different structs
+ *	use a same method in common. Also for keeping an standard
+ */
+
+type Figures2D interface {
+	area() float64
+}
+
+type square struct {
+	base float64
+}
+
+type triangle struct {
+	base   float64
+	height float64
+}
+
+func (sq square) area() float64 {
+	return sq.base * sq.base
+}
+
+// The func area() can be repeated only cause they are methods reffering to an interface
+func (trian triangle) area() float64 {
+	return trian.base * trian.height
+}
+
+func main() {
+	mySquare := square{base: 2}
+	myTriangle := triangle{base: 2, height: 3}
+
+	fmt.Println("Area square: ", mySquare.area())
+	fmt.Println("Area triangle: ", myTriangle.area())
+
+	// Interfaces slice
+	myInterfaceSlice := []interface{}{"Nico", 190, "Boca", true}
+	fmt.Println(myInterfaceSlice...)
+}
