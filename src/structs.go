3c8ecd1a1c6ec515940645d7d4b67f017effe70f
NicoBarragan <nbarragan01@gmail.com>
2022-09-21 17:11:24 -0300

    feat(structs.go): added stringer to car struct

--- src/structs.go
+++ src/structs.go
@@ -10,6 +10,14 @@ type car struct {
 	seating int
 }
 
+// Stringers: a func that works as interface for printing
+// Is efficient than having to write always a dif print statement
+// When printing, it will always have this format
+func (car car) String() string {
+	// sprintf will create a str but won't print on console directly
+	return fmt.Sprintf("My car is a %s %s, from %d with %d seats", car.color, car.brand, car.year, car.seating)
+}
+
 func main() {
 	// create an instance of a struct
 	myCar := car{brand: "Ford", year: 2020, seating: 4}
@@ -23,4 +31,10 @@ func main() {
 	fmt.Println("otherCar: ", otherCar)
 	// Output:
 	// otherCar:  {Ferrari 0  0}
+
+	fmt.Println(myCar)
+	// My car is a  Ford, from 2020 with 4 seats
+	fmt.Println(otherCar)
+	// My car is a  Ferrari, from 0 with 0 seats
+
 }
